019d9655120ca72d5bc0232390461313070be583
iescandon <iescan4@gmail.com>
2023-04-30 21:21:49 -0500

    removed loading

--- pages/player.tsx
+++ pages/player.tsx
@@ -4,11 +4,11 @@ import Carta from "../components/carta";
 export default function Player() {
   const [randomNumberArray, setRandomNumberArray] =
     useState<Array<number> | null>(null);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+  // const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     generateTabla();
-    setIsLoading(false);
+    // setIsLoading(false);
   }, []);
 
   const generateRandomNumber = (num: number) =>
@@ -30,27 +30,27 @@ export default function Player() {
 
   return (
     <section className="flex flex-col items-center justify-center md:p-4 md:min-h-screen">
-      {!isLoading && (
+      {/* {!isLoading && (
+        <> */}
+      <img className="md:shadow-3xl" src="/images/title.png" alt="title" />
+      <div className="grid grid-cols-4 grid-rows-4 border border-4 border-white md:shadow-3xl">
         <>
-          <img className="md:shadow-3xl" src="/images/title.png" alt="title" />
-          <div className="grid grid-cols-4 grid-rows-4 border border-4 border-white md:shadow-3xl">
-            <>
-              {randomNumberArray?.map((num, i) => (
-                <div
-                  key={`div-${i}`}
-                  className="border border-4 border-white relative hover:cursor-pointer"
-                >
-                  <Carta
-                    key={i}
-                    cardNum={num}
-                    generateRandomNumber={generateRandomNumber}
-                  />
-                </div>
-              ))}
-            </>
-          </div>
+          {randomNumberArray?.map((num, i) => (
+            <div
+              key={`div-${i}`}
+              className="border border-4 border-white relative hover:cursor-pointer"
+            >
+              <Carta
+                key={i}
+                cardNum={num}
+                generateRandomNumber={generateRandomNumber}
+              />
+            </div>
+          ))}
         </>
-      )}
+      </div>
+      {/* </>
+      )} */}
     </section>
   );
 }
