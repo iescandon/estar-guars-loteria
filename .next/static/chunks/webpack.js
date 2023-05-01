35273d69a70da50c9e4bb80fdf68da28f0925a5e
iescandon <iescan4@gmail.com>
2023-04-30 22:38:27 -0500

    Added tabla component back in

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f22d517ce737255e"; }
+/******/ 		__webpack_require__.h = function() { return "fdb906b4bd539e95"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
