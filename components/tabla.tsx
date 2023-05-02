51741c1c96d8e7066578c07142fb0643b0ec67d6
iescandon <iescan4@gmail.com>
2023-05-02 16:05:08 -0500

    removed skeleton and added svg loader

--- components/tabla.tsx
+++ components/tabla.tsx
@@ -8,24 +8,17 @@ type TablaProps = {
 export default function Tabla({ isLoading, randomNumberArray }: TablaProps) {
   return (
     <>
-      <div className="flex flex-col">
+      <div
+        className={`flex flex-col ${
+          isLoading ? "invisible h-0 w-0 overflow-hidden" : "visible"
+        }`}
+      >
         <img
-          className={`md:shadow-3xl ${
-            isLoading ? "invisible h-0 overflow-hidden" : "visible"
-          }`}
+          className="md:shadow-3xl"
           src="/images/title.png"
           alt="header of bingo card"
         />
-        <div
-          className={`w-[475px] h-[154px] bg-gray-200 ${
-            isLoading ? "show" : "hidden"
-          }`}
-        ></div>
-        <div
-          className={`grid grid-cols-4 grid-rows-4 md:shadow-3xl border border-4 border-white bg-white ${
-            isLoading ? "invisible h-0 overflow-hidden" : "visible"
-          }`}
-        >
+        <div className="grid grid-cols-4 grid-rows-4 md:shadow-3xl border border-4 border-white bg-white">
           <>
             {randomNumberArray?.map((num, i) => (
               <div
@@ -35,22 +28,6 @@ export default function Tabla({ isLoading, randomNumberArray }: TablaProps) {
                 <Carta key={i} cardNum={num} clickable={true} />
               </div>
             ))}
-          </>
-        </div>
-        <div
-          className={`grid grid-cols-4 grid-rows-4 md:shadow-3xl border border-4 border-white bg-white ${
-            isLoading ? "show" : "hidden"
-          }`}
-        >
-          <>
-            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].map(
-              (num, i) => (
-                <div
-                  key={`div-${i}`}
-                  className="w-[118px] h-[179px] border border-4 border-white relative bg-gray-200"
-                ></div>
-              )
-            )}
           </>
         </div>
       </div>
