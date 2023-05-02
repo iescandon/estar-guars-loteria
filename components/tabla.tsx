d1f72a5392e9cdfee994a698b2f292e5480c44fd
iescandon <iescan4@gmail.com>
2023-05-02 15:40:24 -0500

    other skelton to view on mobile

--- components/tabla.tsx
+++ components/tabla.tsx
@@ -8,38 +8,24 @@ type TablaProps = {
 export default function Tabla({ isLoading, randomNumberArray }: TablaProps) {
   return (
     <>
-      {/* skeleton */}
-      <div
-        className={`flex flex-col animate-pulse ${
-          isLoading ? "show" : "hidden"
-        }`}
-      >
-        <div className="w-[475px] h-[154px] bg-zinc-200"></div>
-        <div className="grid grid-cols-4 grid-rows-4 md:shadow-3xl border border-4 border-white bg-white">
-          <>
-            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].map(
-              (num, i) => (
-                <div
-                  key={`div-${i}`}
-                  className="w-[118px] h-[179px] border border-4 border-white relative bg-gray-200"
-                ></div>
-              )
-            )}
-          </>
-        </div>
-      </div>
-      {/* tabla */}
-      <div
-        className={`flex flex-col ${
-          isLoading ? "invisible w-0 overflow-hidden" : "visible"
-        }`}
-      >
+      <div className="flex flex-col">
         <img
-          className="md:shadow-3xl"
+          className={`md:shadow-3xl ${
+            isLoading ? "invisible h-0 overflow-hidden" : "visible"
+          }`}
           src="/images/title.png"
           alt="header of bingo card"
         />
-        <div className="grid grid-cols-4 grid-rows-4 md:shadow-3xl border border-4 border-white bg-white">
+        <div
+          className={`w-[475px] h-[154px] bg-gray-200 ${
+            isLoading ? "show" : "hidden"
+          }`}
+        ></div>
+        <div
+          className={`grid grid-cols-4 grid-rows-4 md:shadow-3xl border border-4 border-white bg-white ${
+            isLoading ? "invisible h-0 overflow-hidden" : "visible"
+          }`}
+        >
           <>
             {randomNumberArray?.map((num, i) => (
               <div
@@ -49,6 +35,22 @@ export default function Tabla({ isLoading, randomNumberArray }: TablaProps) {
                 <Carta key={i} cardNum={num} clickable={true} />
               </div>
             ))}
+          </>
+        </div>
+        <div
+          className={`grid grid-cols-4 grid-rows-4 md:shadow-3xl border border-4 border-white bg-white ${
+            isLoading ? "show" : "hidden"
+          }`}
+        >
+          <>
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].map(
+              (num, i) => (
+                <div
+                  key={`div-${i}`}
+                  className="w-[118px] h-[179px] border border-4 border-white relative bg-gray-200"
+                ></div>
+              )
+            )}
           </>
         </div>
       </div>
